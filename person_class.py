068aea04521aea49c5494fea259b86a663ec4b12
cynguyen <cynthia_nguyen25@hotmail.com>
2020-10-12 15:23:43 -0600

    created new module
    module for person class containing name, email and buying for

---
+++ person_class.py
@@ -0,0 +1,8 @@
+class Person:
+
+    def __init__(person, name, email, buying_for):
+        person.name = name
+        person.email = email
+        person.buying_for = buying_for
+        
+        
