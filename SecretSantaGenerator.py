d83c30563124ecd91d9ca560931df6935be3304d
Dell <ddsayson@hotmail.com>
2021-09-17 14:53:17 -0600

    added check so buyers and receivers only appear once; missing case to handle Linn and Maddie

--- SecretSantaGenerator.py
+++ SecretSantaGenerator.py
@@ -14,47 +14,72 @@ random.shuffle(namelist)
 # main function
 def main():
 
-    # [  ] TODO record matches last time
-    # [  ] TODO print last secret santa matches
+    # [   ] TODO record matches last time
+    # [   ] TODO print last secret santa matches
+    # [   ] TODO remove special case for Maddison
+    # [   ] TODO check new matches are different from the previous matches
+    # [   ] TODO check new matches don't overlap
+    #   [ Y ] TODO make sure each buyer appears once
+    #   [ Y ] TODO make sure each receiver appears once
+    #   [   ] TODO make sure no one has themselves
 
-    print('last time')
-    print('---------')
+    buyersAppearOnce=False
+    receiversAppearOnce=False
+    while not buyersAppearOnce and not receiversAppearOnce:
+        print('last time')
+        print('---------')
 
-    print()
-    idx = 0
+        print()
+        idx = 0
 
-    for name in namelist:
-        number = numbers[idx]
-        if idx == 8: 
-            number2 = numbers[0]
-        else:
-            number2 = numbers[idx + 1]
-        print(name, "You are number:", number, "You are buying for number:", number2)
-        make_list(name, number)
-        make_buying_list(number, number2)
-        idx = idx + 1
-    
-    maddieidx = finallist.index("Maddison")+1
-    print("Linn You are number: 10 You are buying for number:", maddieidx)
-	
-    print()
-
-    for x in range(len(finallist)):
-        y = buyinglist[x] - 1
-        buyingfor = finallist[y]
-        buyingidx = buyinglist[x]
-        if buyingfor == "Maddison": 
-            buyingfor = "Linn"
-            buyingidx = 10
-            
-        # send emails here
+        for name in namelist:
+            number = numbers[idx]
+            if idx == 8: 
+                number2 = numbers[0]
+            else:
+                number2 = numbers[idx + 1]
+            print(name, "You are number:", number, "You are buying for number:", number2)
+            make_list(name, number)
+            make_buying_list(number, number2)
+            idx = idx + 1
         
-        print(x+1, finallist[x], "is buying for" , buyingfor, buyingidx)
-    
-    print("10 Linn is buying for Maddison", maddieidx)
-    
-    # [  ] TODO check new matches are different from the previous matches
-    # [  ] TODO check new matches don't overlap
+        maddieidx = finallist.index("Maddison")+1
+        print("Linn You are number: 10 You are buying for number:", maddieidx)
+    	
+        print()
+
+        for x in range(len(finallist)):
+            y = buyinglist[x] - 1
+            buyingfor = finallist[y]
+            buyingidx = buyinglist[x]
+            if buyingfor == "Maddison": 
+                buyingfor = "Linn"
+                buyingidx = 10
+                
+            # send emails here
+            
+            print(x+1, finallist[x], "is buying for" , buyingfor, buyingidx)
+        
+        print("10 Linn is buying for Maddison", maddieidx)
+        
+        buyersAppearOnce=item_appears_once(finallist)
+        receiversAppearOnce=item_appears_once(buyinglist)
+
+        buyerList=finallist
+        receiverList=[buyerList[receiverIndex-1] for receiverIndex in buyinglist] # note buyerList!
+        print("buyers:", buyerList)
+        print("receivers:", receiverList)
+
+    print('Matches Valid')
+
+def item_appears_once(inputList):
+    testSet=set()
+    for item in inputList:
+        if item in testSet:
+            return False
+        testSet.add(item)
+    if len(testSet) == len(inputList):
+        return True
 
 # function to create the list of names        
 def make_list(x, y):
