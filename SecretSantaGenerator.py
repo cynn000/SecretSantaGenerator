6c30718d36b32c455cbe08f1466bffca84c5181a
Dell <ddsayson@hotmail.com>
2021-09-20 22:08:41 -0600

    added while loop

--- SecretSantaGenerator.py
+++ SecretSantaGenerator.py
@@ -38,18 +38,17 @@ linn_email = "linntao72888@gmail.com"
 # main function to do all the magic
 def main():
     
+    # [   ] TODO add Linn to the namelist
     # [   ] TODO remove Linn and Maddison hardcoding
     # [   ] TODO record matches last time
     # [   ] TODO print last secret santa matches
     # [   ] TODO remove special case for Maddison
     # [   ] TODO check new matches are different from the previous matches
     # [   ] TODO check new matches don't overlap
-    #   [   ] TODO make sure each buyer appears once
-    #   [   ] TODO make sure each receiver appears once
-    #   [   ] TODO make sure no one has themselves
-    
-    # set index to 0
-    idx = 0
+    #   [   ] TODO ensure each participant only appears once (one person isn't buying for two or more people)
+    #   [   ] TODO ensure each receiver only appears once (one person isn't receiving from more than one person)
+    #   [   ] TODO ensure each participant is not buying for themselves
+    #   [   ] TODO ensure Linn does not have Maddison
     
     # creating a new person class with name, email, and unknown buying for until determined
     # hardcoded in who Linn is buying for
@@ -64,107 +63,117 @@ def main():
     nic = person_class.Person("Nic", nic_email, UNKNOWN)
     linn = person_class.Person("Linn", linn_email, "Maddison")
 
+    while ?:
+        # set index to 0
+        idx = 0
 
-    personList = []
-    personList.append(cynthia)
-    personList.append(danielle)
-    personList.append(dell)
-    personList.append(maddison)
-    personList.append(oscar)
-    personList.append(brandon)
-    personList.append(michelle)
-    personList.append(loann)
-    personList.append(nic)
-    personList.append(linn)
+        personList = []
+        personList.append(cynthia)
+        personList.append(danielle)
+        personList.append(dell)
+        personList.append(maddison)
+        personList.append(oscar)
+        personList.append(brandon)
+        personList.append(michelle)
+        personList.append(loann)
+        personList.append(nic)
+        personList.append(linn)
 
-    # for each name in list
-    for name in namelist:
-    
-        # assign a number from the numbers list
-        number = numbers[idx]
+        # for each name in list
+        for name in namelist:
         
-        # if the number chosen is 8
-        if idx == 8:  
-            # set number2 (which is the number of the person who we are buying for) to the very first number in the list
-            # this means that the very last person will buy for the very first person (watch video for reference)
-            number2 = numbers[0]
-        else:
-            # else then set number2 to the next number in the number list
-            number2 = numbers[idx + 1]
+            # assign a number from the numbers list
+            number = numbers[idx]
             
-        # printing for testing    
-        # print(name, "You are number:", number, "You are buying for number:", number2)
-        
-        # create the name list by passing in the name and their own number`
-        make_list(name, number)
-        
-        # create the buying for list with the number and number2 (which is the number of the person they will be buying for)
-        make_buying_list(number, number2)
-        
-        # increase index to move on to the next number in the number list
-        idx = idx + 1
-    
-    # calculate Maddion's number
-    maddieidx = finallist.index("Maddison")+1
-    
-    # printing for testing. Hardcoded Linn to buy for Maddison. 
-    # print("Linn You are number: 10 You are buying for number:", maddieidx)
-	
-    # print new line
-    # print()
-    
-    # loop through the finalist length to list out the names and who they are buying for
-    for x in range(len(finallist)):
-    
-        # set y as the number of who they are buying for minus 1 due to index
-        y = buyinglist[x] - 1
-        
-        # participant name
-        participant = finallist[x]
-        
-        # buying_for name obtained from the final list which is the next person in the list
-        buyingfor = finallist[y]
-        
-        # buying_for person's number
-        buyingidx = buyinglist[x]
-        
-        # if the buying for name is equal to Maddison
-        if buyingfor == "Maddison": 
-        
-            # set it so whoever has Maddison will now have Linn
-            buyingfor = "Linn"
-            buyingidx = 10
+            # if the number chosen is 8
+            if idx == 8:  
+                # set number2 (which is the number of the person who we are buying for) to the very first number in the list
+                # this means that the very last person will buy for the very first person (watch video for reference)
+                number2 = numbers[0]
+            else:
+                # else then set number2 to the next number in the number list
+                number2 = numbers[idx + 1]
+                
+            # printing for testing    
+            # print(name, "You are number:", number, "You are buying for number:", number2)
             
-        print(x+1, finallist[x], "is buying for" , buyingfor, buyingidx)
+            # create the name list by passing in the name and their own number`
+            make_list(name, number)
+            
+            # create the buying for list with the number and number2 (which is the number of the person they will be buying for)
+            make_buying_list(number, number2)
+            
+            # increase index to move on to the next number in the number list
+            idx = idx + 1
         
-        # setting the buying_for variable as it is now determined
-        if participant == "Cynthia": cynthia.buying_for = buyingfor
-        if participant == "Danielle": danielle.buying_for = buyingfor
-        if participant == "Dell": dell.buying_for = buyingfor
-        if participant == "Maddison": maddison.buying_for = buyingfor
-        if participant == "Oscar": oscar.buying_for = buyingfor
-        if participant == "Brandon": brandon.buying_for = buyingfor
-        if participant == "Michelle": michelle.buying_for = buyingfor
-        if participant == "Loann": loann.buying_for = buyingfor
-        if participant == "Nic": nic.buying_for = buyingfor
-    
-    # hardcoded Linn to buy for Maddison. We baaaaaaad
-    # print("10 Linn is buying for Maddison", maddieidx)
-    
+        # calculate Maddion's number
+        maddieidx = finallist.index("Maddison")+1
+        
+        # printing for testing. Hardcoded Linn to buy for Maddison. 
+        # print("Linn You are number: 10 You are buying for number:", maddieidx)
+    	
+        # print new line
+        # print()
+        
+        # loop through the finalist length to list out the names and who they are buying for
+        for x in range(len(finallist)):
+        
+            # set y as the number of who they are buying for minus 1 due to index
+            y = buyinglist[x] - 1
+            
+            # participant name
+            participant = finallist[x]
+            
+            # buying_for name obtained from the final list which is the next person in the list
+            buyingfor = finallist[y]
+            
+            # buying_for person's number
+            buyingidx = buyinglist[x]
+            
+            # if the buying for name is equal to Maddison
+            if buyingfor == "Maddison": 
+            
+                # set it so whoever has Maddison will now have Linn
+                buyingfor = "Linn"
+                buyingidx = 10
+                
+            print(x+1, finallist[x], "is buying for" , buyingfor, buyingidx)
+            
+            # setting the buying_for variable as it is now determined
+            if participant == "Cynthia": cynthia.buying_for = buyingfor
+            if participant == "Danielle": danielle.buying_for = buyingfor
+            if participant == "Dell": dell.buying_for = buyingfor
+            if participant == "Maddison": maddison.buying_for = buyingfor
+            if participant == "Oscar": oscar.buying_for = buyingfor
+            if participant == "Brandon": brandon.buying_for = buyingfor
+            if participant == "Michelle": michelle.buying_for = buyingfor
+            if participant == "Loann": loann.buying_for = buyingfor
+            if participant == "Nic": nic.buying_for = buyingfor
+        
+        # hardcoded Linn to buy for Maddison. We baaaaaaad
+        # print("10 Linn is buying for Maddison", maddieidx)
+
+        # ensure each participant only appears once
+        # ensure each receiver(each buyingFor) only appears once
+        # ensure each participant is not buying for themselves
+
+        # ensure Linn does not have Maddison
+
+    print('matches valid.')
+    print('sending emails...')
+
     '''
     print(SCRIPT_START)
     message=''
     mailClient=sendEmail.MailClient()
     for buyer in personList:
-    	message=mailClient.getMessage(buyer.name, buyer.buying_for)
-    	if (buyer==dell or buyer==danielle or buyer==cynthia):
-    		mailClient.send(buyer.email, message)
-    	print('sent to: '+buyer.name)
-    	message=''
+        message=mailClient.getMessage(buyer.name, buyer.buying_for)
+        if (buyer==dell or buyer==danielle or buyer==cynthia):
+            mailClient.send(buyer.email, message)
+        print('sent to: '+buyer.name)
+        message=''
     print(SCRIPT_END)
     '''
-
-    # send last email for Linn here
 
 # function to create the list of names        
 def make_list(x, y):
